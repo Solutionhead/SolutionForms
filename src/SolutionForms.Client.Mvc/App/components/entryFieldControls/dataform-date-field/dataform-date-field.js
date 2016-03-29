4f95d3836d939dca69b90d760859f9ea5d3c38a2
Vinney K <vinneyk@live.com>
2016-03-29 11:58:04 -0600

    Added view model file for date field component missing from previous commit.

--- src/SolutionForms.Client.Mvc/App/components/entryFieldControls/dataform-date-field/dataform-date-field.js
+++ src/SolutionForms.Client.Mvc/App/components/entryFieldControls/dataform-date-field/dataform-date-field.js
@@ -1,1 +1,32 @@
-﻿
+﻿var base = require('controls/basicEntryField');
+require('koExtenders/knockout.extenders.moment');
+
+function DateFieldViewModel(params) {
+  if(!(this instanceof DateFieldViewModel)) { return new DateFieldViewModel(params); }
+
+  var self = this;
+
+  base.call(this, params, true);
+
+  self.selectedDate = ko.observable().extend({ moment: 'M/D/YYYY' });
+  self.userResponse = ko.computed({
+    read: function() {
+      return self.selectedDate.toAbsoluteDateISOString();
+    },
+    write: function(value) {
+      self.selectedDate(value);
+    }
+  });
+
+  params.context(self);
+
+  return self;
+}
+
+DateFieldViewModel.prototype = base.prototype;
+
+module.exports = {
+  viewModel: DateFieldViewModel,
+  template: require('./dataform-date-field.html'),
+  synchronous: true,
+}
