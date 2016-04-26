8026c7e276831fdef45fe89fad2e30462240684b
Vinney K <vinneyk@solutionhead.com>
2016-04-26 05:49:46 -0600

    Introduced hidden-field entry component which needs to set the container type so as not to display the label and ui-hint elements.

---
+++ src/SolutionForms.Client.Mvc/App/components/field-controls/dataform-hidden-field/dataform-hidden-field.js
@@ -0,0 +1,26 @@
+﻿var base = require('controls/basicEntryField'),
+  componentName = 'hidden-field';
+
+function HiddenField(params) {
+  if (!(this instanceof HiddenField)) { return new HiddenField(params); }
+
+  var self = base.call(this, params, true);
+  self.FieldType = "container";
+  return self;
+}
+
+var config = {
+  viewModel: HiddenField,
+  template: '<span>Hidden Field</span>'
+}
+
+
+module.exports = {
+  viewModel: HiddenField,
+  template: '<input type="hidden" data-bind="value: userResponse"/>',
+  name: 'Hidden Field',
+  componentName: 'hidden-field'
+};
+
+ko.components.register(componentName, module.exports);
+ko.components.register(componentName + '-config', config);
